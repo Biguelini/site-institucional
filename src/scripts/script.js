10c1c00c49a969b90c49145ea83cc03cdfe2b0ac
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-04-01 14:45:30 -0300

    feat: carrossel

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,23 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  let currentIndex = 0;
+  const slides = document.querySelectorAll(".slides img");
+  
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+  
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+  
+  setInterval(nextSlide, 3000); // Altere 3000 para o intervalo desejado em milissegundos (3 segundos neste caso)
+  
+  showSlide(currentIndex);
+});
